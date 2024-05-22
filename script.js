73c160c0241ea5c862dde38344074755529b1ec3
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2024-05-22 16:22:58 +0300

    Add change name bn

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ const GameBoard = (function () {
   ];
 
   let playerOne = {
-    playerName: "Player 1",
+    playerName: "Umar",
     mark: "X",
   };
   let playerTwo = {
-    playerName: "Player 2",
+    playerName: "Zaid",
     mark: "O",
   };
 
-  let currentPlayer;
+  let currentPlayer = playerOne;
+
+  const markBtns = document.querySelectorAll(".cell");
+  const resetBtn = document.querySelector("#reset-btn");
+  const turnHeader = document.querySelector("#turn");
+  const changeNameModal = document.querySelector("dialog");
+  const changeNameBtn = document.querySelector("#change-name-btn");
 
   function startGame() {
     currentPlayer = playerOne;
@@ -77,6 +83,12 @@ const GameBoard = (function () {
       return true;
   }
 
+  function checkWinner() {
+    if (checkAllSame()) {
+      return `${currentPlayer.playerName} wins!`;
+    }
+  }
+
   function switchPlayer() {
     if (currentPlayer == playerOne) currentPlayer = playerTwo;
     else if (currentPlayer == playerTwo) currentPlayer = playerOne;
@@ -92,12 +104,6 @@ const GameBoard = (function () {
 
     console.clear();
     console.table(board);
-    if (checkAllSame()) {
-      resetGame();
-      return `${currentPlayer.playerName} wins!`;
-    }
-
-    switchPlayer();
   }
 
   function resetGame() {
@@ -107,7 +113,37 @@ const GameBoard = (function () {
       ["-", "-", "-"],
     ];
     currentPlayer = playerOne;
+    displayTurn();
+    Array.from(markBtns).forEach((btn) => {
+      btn.textContent = "";
+    });
   }
+
+  Array.from(markBtns).forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let row = btn.dataset.row;
+      let column = btn.dataset.column;
+      addMark(row, column);
+      btn.textContent = currentPlayer.mark;
+      console.log(currentPlayer);
+      console.log(checkWinner());
+      displayTurn();
+      switchPlayer();
+    });
+  });
+
+  function displayTurn() {
+    if (currentPlayer == playerOne)
+      turnHeader.textContent = `${playerTwo.playerName}'s Turn`;
+    else if (currentPlayer == playerTwo)
+      turnHeader.textContent = `${playerOne.playerName}'s Turn`;
+  }
+
+  resetBtn.addEventListener("click", resetGame);
+
+  changeNameBtn.addEventListener("click", function () {
+    changeNameModal.close();
+  });
 
   return {
     addMark: addMark,
